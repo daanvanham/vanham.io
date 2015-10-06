5f99497d702712b1ca2fa7dfc8210a63da9a4206
Daan van Ham <daan@vanham.io>
2015-10-06 22:28:47 +0200

    added 404 page

--- api/web/index.js
+++ api/web/index.js
@@ -11,6 +11,14 @@ exports.register = function(server, options, next) {
 
 	server.route({
 		method: 'GET',
+		path: '/404',
+		handler: function(request, reply) {
+			return reply.view('404').code(404);
+		}
+	});
+
+	server.route({
+		method: 'GET',
 		path: '/{friendlyURL}',
 		handler: function(request, reply) {
 			return reply.view('base');
