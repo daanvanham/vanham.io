e88e159465782ca2b3a832f86357766ae04a6f28
Daan van Ham <daan@vanham.io>
2015-10-06 23:45:39 +0200

    updated style task

--- gulp/task/style.js
+++ gulp/task/style.js
@@ -3,7 +3,6 @@
 module.exports = function(stream, devour) {
 	return stream
 		.pipe(devour.plugin('myth'))
-		.pipe(devour.write('./css'))
 		.pipe(devour.plugin('minify-css'))
 		.pipe(devour.plugin('rename', devour.min))
 		.pipe(devour.write('./css'));
