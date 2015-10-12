9104da75c423d3e01f7051b944af5767e4e77938
Daan van Ham <daan@vanham.io>
2015-10-12 18:20:26 +0200

    created function which return the actual handler callback

--- api/web/index.js
+++ api/web/index.js
@@ -1,36 +1,34 @@
 'use strict';
 
+function handler(view) {
+	return function(request, reply) {
+		reply.view(view).header('Content-Type', 'text/html; charset=utf-8');
+	};
+}
+
 exports.register = function(server, options, next) {
 	server.route({
 		method: 'GET',
 		path: '/',
-		handler: function(request, reply) {
-			return reply.view('base').header('Content-Type', 'text/html; charset=utf-8');
-		}
+		handler: handler('base')
 	});
 
 	server.route({
 		method: 'GET',
 		path: '/404',
-		handler: function(request, reply) {
-			return reply.view('404').code(404).header('Content-Type', 'text/html; charset=utf-8');
-		}
+		handler: handler('404')
 	});
 
 	server.route({
 		method: 'GET',
 		path: '/{friendlyURL}',
-		handler: function(request, reply) {
-			return reply.view('base').header('Content-Type', 'text/html; charset=utf-8');
-		}
+		handler: handler('base')
 	});
 
 	server.route({
 		method: 'GET',
 		path: '/template/detail',
-		handler: function(request, reply) {
-			return reply.view('detail').header('Content-Type', 'text/html; charset=utf-8');
-		}
+		handler: handler('detail')
 	});
 
 	next();
