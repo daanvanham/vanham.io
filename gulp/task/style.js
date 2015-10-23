504d598ab8cc1ef555364741d4fa82e269b0e2b8
Daan van Ham <daan@vanham.io>
2015-10-23 20:29:58 +0200

    generate a spoiler for images in the combined stylesheet in the gulp task

--- gulp/task/style.js
+++ gulp/task/style.js
@@ -1,10 +1,22 @@
 'use strict';
 
+function spoiler() {
+	var alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+='.split(''),
+		result = [alphabet[Math.round(Math.random() * (alphabet.length - 1))]];
+
+	while (result.length <= 5) {
+		result.push(alphabet[Math.round(Math.random() * (alphabet.length - 1))]);
+	}
+
+	return result.join('');
+}
+
 module.exports = function(stream, devour) {
 	return stream
 		.pipe(devour.plugin('myth'))
 		.pipe(devour.plugin('minify-css'))
 		.pipe(devour.pipe('combine', 'css'))
 		.pipe(devour.plugin('rename', devour.min))
+		.pipe(devour.plugin('replace', /url\(\/static\/img/g, 'url(/static/' + spoiler() + '/img'))
 		.pipe(devour.write('./css'));
 };
