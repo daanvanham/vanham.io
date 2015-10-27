e2b70fc2e931dcba22cc8b701403907f4f4b7981
Daan van Ham <daan@vanham.io>
2015-10-27 21:46:58 +0100

    replaced gulp-html-minifier with gulp-minify-html

--- gulp/task/html.js
+++ gulp/task/html.js
@@ -3,7 +3,7 @@
 module.exports = function(stream, devour) {
 	return stream
 		.pipe(devour.write('./html'))
-		.pipe(devour.plugin('html-minifier'))
+		.pipe(devour.plugin('minify-html'))
 		.pipe(devour.plugin('rename', devour.min))
 		.pipe(devour.write('./html'));
 };
