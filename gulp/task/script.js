fd28c2be74732044dcb0d7b66c2360a400380ef1
Daan van Ham <daan@vanham.io>
2015-11-25 23:10:52 +0100

    use devour for the sourcemap plugin

--- gulp/task/script.js
+++ gulp/task/script.js
@@ -1,14 +1,12 @@
 'use strict';
 
-const sourcemaps = require('gulp-sourcemaps');
-
 module.exports = (stream, devour) => {
 	return stream
-		.pipe(sourcemaps.init())
+		.pipe(devour.plugin('sourcemaps').init())
 		.pipe(devour.write('./js'))
 		.pipe(devour.pipe('combine', 'js'))
 		.pipe(devour.plugin('uglify'))
 		.pipe(devour.plugin('rename', devour.min))
-		.pipe(sourcemaps.write('./'))
+		.pipe(devour.plugin('sourcemaps').write('./'))
 		.pipe(devour.write('./js'));
 };
