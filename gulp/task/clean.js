cd67b1cf323d038255cfabf9b1ddf97a13876acb
Daan van Ham <daan@vanham.io>
2016-02-10 21:19:04 +0100

    cleaned up Gulp tasks

--- gulp/task/clean.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const del = require('del');
-
-module.exports = (stream, devour) => {
-	return del('./client/build');
-};
--- gulp/task/script.js
+++ gulp/task/script.js
@@ -2,11 +2,9 @@
 
 module.exports = (stream, devour) => {
 	return stream
-		.pipe(devour.plugin('sourcemaps').init())
 		.pipe(devour.pipe('combine', 'js'))
 		.pipe(devour.plugin('uglify'))
 		.pipe(devour.pipe('selector', 'js'))
 		.pipe(devour.plugin('rename', devour.min))
-		.pipe(devour.plugin('sourcemaps').write('./'))
 		.pipe(devour.write('./js'));
 };
